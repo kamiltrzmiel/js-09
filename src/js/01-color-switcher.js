38308f826e58037fa95d0530fe751506f58db210
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-04-09 21:49:32 +0200

    task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let colorIntervalId;
+
+function getHexRandomColor() {
+  const randomNum = Math.floor(Math.random() * 16777215);
+  const hexString = randomNum.toString(16).padStart(6, '0');
+  return '#' + hexString;
+}
+//START COLOR SWITCHER
+function startCS() {
+  colorIntervalId = setInterval(() => {
+    document.body.style.backgroundColor = getHexRandomColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+//STOP COLOR SWITCHER
+function stopCS() {
+  clearInterval(colorIntervalId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+startBtn.addEventListener('click', startCS);
+stopBtn.addEventListener('click', stopCS);
